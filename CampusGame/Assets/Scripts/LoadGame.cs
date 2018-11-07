8499a2b37914a4d4874e92e49be392bd17f3f799
nackster <realife100@gmail.com>
2018-11-06 21:16:54 -0600

    MainMenu
    MainMenu Added

---
+++ CampusGame/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadGame : MonoBehaviour 
+{
+
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
