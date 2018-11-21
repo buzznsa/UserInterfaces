c14f6458fe5e1d5f919bcb204e1d7a74ea97517e
buzznsa <Buzzn.sa@gmail.com>
2018-11-21 13:18:59 -0600

    upload
    Upload CampusGame

---
+++ CampusGame/Assets/Scenes/Test/Jordan/DoorWay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorWay : MonoBehaviour {
+
+    public GameObject guiObject;
+
+    public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+        guiObject.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void onTriggerStay (Collider other) {
+
+        if(other.gameObject.tag == "Player")
+        {
+            guiObject.SetActive(true);
+            if(guiObject.activeInHierarchy == true && Input.GetButtonDown("use"))
+            {
+                SceneManager.LoadScene(levelToLoad);
+            }
+
+        }
+	}
+
+    private void OnTriggerExit()
+    {
+        guiObject.SetActive(false);
+
+    }
+}
---
+++ CampusGame/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadGame : MonoBehaviour 
+{
+
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("7.1CharacterControllers");
+    }
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
